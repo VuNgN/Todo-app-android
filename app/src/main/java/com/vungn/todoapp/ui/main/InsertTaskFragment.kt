5bea35aa04c56bc04f83cca011abaa4bddcef48b
vu.nguyengoc <vu.nguyengoc1@vti.com.vn>
2022-08-12 13:57:27 +0700

    Implement: Insert task fragment

--- app/src/main/java/com/vungn/todoapp/ui/main/InsertTaskFragment.kt
+++ app/src/main/java/com/vungn/todoapp/ui/main/InsertTaskFragment.kt
@@ -1,12 +1,14 @@
 package com.vungn.todoapp.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.vungn.todoapp.R
 import com.vungn.todoapp.databinding.FragmentInsertTaskBinding
 
@@ -23,6 +25,40 @@ class InsertTaskFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupUi()
+        handleEvent()
+    }
+
+    private fun handleEvent() {
+        binding.apply {
+            dueDate.setEndIconOnClickListener {
+                calendarPicker.visibility = if (calendarPicker.visibility == View.GONE)
+                    View.VISIBLE else View.GONE
+            }
+
+            dueDateEditText.setOnClickListener {
+                calendarPicker.visibility = if (calendarPicker.visibility == View.GONE)
+                    View.VISIBLE else View.GONE
+            }
+
+            calendarPicker.setOnDateChangeListener { _, year, month, dayOfMonth ->
+                dueDateEditText.setText(
+                    resources.getString(
+                        R.string.due_date,
+                        dayOfMonth.toString(),
+                        (month + 1).toString(),
+                        year.toString()
+                    )
+                )
+            }
+
+            buttonBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
+
+            dueDate.setOnFocusChangeListener { v, hasFocus ->
+                Log.d("aaaaaa", "handleEvent: " + hasFocus)
+            }
+        }
     }
 
     private fun setupUi() {
@@ -30,6 +66,7 @@ class InsertTaskFragment : Fragment() {
         val dropdownAdapter =
             ArrayAdapter(requireContext(), R.layout.item_dropdown_insert_task, timeTypes)
         binding.apply {
+            calendarPicker.visibility = View.GONE
             (remindMe.editText as AutoCompleteTextView).setAdapter(dropdownAdapter)
             (remindMe.editText as AutoCompleteTextView).setText(dropdownAdapter.getItem(3), false)
 
