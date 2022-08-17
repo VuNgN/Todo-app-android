e49d3d9129dc2e9fcd26e70a837db3666265fa75
vu.nguyengoc <vu.nguyengoc1@vti.com.vn>
2022-08-17 14:13:16 +0700

    Implement: First time run app

--- app/src/main/java/com/vungn/todoapp/ui/authentication/activity/AuthenticationActivity.kt
+++ app/src/main/java/com/vungn/todoapp/ui/authentication/activity/AuthenticationActivity.kt
@@ -2,20 +2,49 @@ package com.vungn.todoapp.ui.authentication.activity
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Rect
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.ContextCompat.startActivity
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import androidx.navigation.navOptions
 import com.vungn.todoapp.R
+import com.vungn.todoapp.databinding.ActivityAuthenticationBinding
 import com.vungn.todoapp.ui.main.activity.MainActivity
 
 class AuthenticationActivity : AppCompatActivity() {
+    private lateinit var prefs: SharedPreferences
+    private lateinit var binding: ActivityAuthenticationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
+        binding = ActivityAuthenticationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        prefs = getSharedPreferences("com.vungn.todoapp", MODE_PRIVATE)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (prefs.getBoolean("firstrun", true)) {
+            prefs.edit().putBoolean("firstrun", false).apply()
+            changeStartDestination(R.id.loginFragment, R.id.getStartedFragment)
+        } else {
+            changeStartDestination(R.id.getStartedFragment, R.id.loginFragment)
+        }
+    }
+
+    private fun changeStartDestination(popup: Int, navigate: Int) {
+        val navOptions = NavOptions.Builder().setPopUpTo(popup, true).build()
+        Navigation.findNavController(binding.myNavHostFragment).navigate(navigate, null, navOptions)
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
