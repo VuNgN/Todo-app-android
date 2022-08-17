69c6c5dedee8fd434b119d32e89599acf75aa2ad
vu.nguyengoc <vu.nguyengoc1@vti.com.vn>
2022-08-17 17:31:40 +0700

    Implement: Check logged in

--- app/src/main/java/com/vungn/todoapp/ui/authentication/verification/contract/implement/VerificationViewModelImpl.kt
+++ app/src/main/java/com/vungn/todoapp/ui/authentication/verification/contract/implement/VerificationViewModelImpl.kt
@@ -5,10 +5,15 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.vungn.todoapp.data.repository.ConfigManager
 import com.vungn.todoapp.ui.authentication.verification.contract.VerificationViewModel
 
 class VerificationViewModelImpl(application: Application) : AndroidViewModel(application),
     VerificationViewModel {
+    private val configManager: ConfigManager by lazy {
+        ConfigManager(application.applicationContext)
+    }
+
     private val otp = MutableLiveData<String>()
     private val navigation = MutableLiveData<Boolean>()
 
@@ -18,8 +23,8 @@ class VerificationViewModelImpl(application: Application) : AndroidViewModel(app
 
     override fun checkOtp() {
         if (otp.value == "0000") {
-
             navigation.postValue(true)
+            configManager.setLoggedIn(true)
         }
     }
 
